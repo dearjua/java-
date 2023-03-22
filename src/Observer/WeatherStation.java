8f9e746dc2e476f15f528f63358704d8369f4f20
li111 <2356980654@qq.com>
2023-03-22 21:18:16 +0800

    test

--- src/Observer/WeatherStation.java
+++ src/Observer/WeatherStation.java
@@ -4,14 +4,13 @@ package Observer;
  * ClassName: WeatherStation <br/>
  * Description: <br/>
  * date: 2023/3/20 20:33<br/>
- *
  * @author li111<br />
  */
 public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
         CurrentConditionsDisplay conditionsDisplay = new CurrentConditionsDisplay(weatherData);
-        weatherData.setMeasurements(80.5f,56.2f,155.2f);
+        weatherData.setMeasurements(90.5f,56.2f,155.2f);
     }
 
 }
